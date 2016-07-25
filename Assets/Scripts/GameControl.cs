c09ead21224064aee0678855517550229d43cc81
Generalkidd <Plasmadragons@hotmail.com>
2016-07-25 15:45:38 -0400

    Initial Commit
    Initial commit of project files

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControl : MonoBehaviour
+{
+    GameObject slender;
+    int count = 0;
+    Time gameTime;
+	// Use this for initialization
+	void Start ()
+    {
+        //slender = (GameObject)Instantiate(GameObject.Find("Slenderman"), new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z - 3.5f), new Quaternion(0,0,0,0));
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(this.transform.rotation.y > 90 || this.transform.rotation.y < -90)
+        {
+            //slender = (GameObject)Instantiate(GameObject.Find("Slenderman"), new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z - 3.5f), new Quaternion(0, 0, 0, 0));
+            //slender.SetActive(false);
+            Destroy(slender);
+            count = 0;
+        }
+        else if(count < 2 && Time.time > 30)
+        {
+            count++;
+            slender = (GameObject)Instantiate(GameObject.Find("Slenderman"), new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z - 3.5f), new Quaternion(0, 0, 0, 0));
+            //slender.SetActive(true);
+            //slender.gameObject.GetComponent<Renderer>().enabled = true;
+        }
+        
+	}
+}
